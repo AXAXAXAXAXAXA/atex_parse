765b3d8d05995897ac57664bd3f82fa8dc46d25f
zadro <zadrot_11112@mail.ru>
2022-10-10 15:16:33 +0300

    v1.3 исправлена работа со временем

--- asyncMain.py
+++ asyncMain.py
@@ -35,8 +35,8 @@ async def get_main_page():
         f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36')
     options.add_argument("--disable-blink-features=AutomationControlled")
 
-    url = 'https://atexplus.net/news/news_category/tehnicheskie-raboty/'
-
+    #url = 'https://atexplus.net/news/news_category/tehnicheskie-raboty/'
+    url = 'C:/Users/zadro/Desktop/%D0%90%D1%80%D1%85%D0%B8%D0%B2%D1%8B%20%D0%A2%D0%B5%D1%85%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B5%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B%20-%20%D0%93%D0%9A%20%D0%90%D0%A2%D0%AD%D0%9A%D0%A1%20-%20%D0%A2%D0%B5%D0%BB%D0%B5%D0%B2%D0%B8%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5,%20%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82,%20%D0%92%D0%B8%D0%B4%D0%B5%D0%BE%D0%BD%D0%B0%D0%B1%D0%BB%D1%8E%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5,%20%D0%A3%D0%BC%D0%BD%D1%8B%D0%B9%20%D0%B4%D0%BE%D0%BC%D0%BE%D1%84%D0%BE%D0%BD%20_%20%D0%9F%D1%80%D0%BE%D0%B2%D0%B0%D0%B9%D0%B4%D0%B5%D1%80%20%D0%B2%20%D0%B3.%20%D0%A0%D1%8B%D0%B1%D0%B8%D0%BD%D1%81%D0%BA%D0%B5.html'
     service = Service('E:\\eldoradoPARSE\\chromedriver\\chromedriver.exe')
     driver = webdriver.Chrome(
         service=service,
@@ -80,7 +80,7 @@ async def send_list(message: types.Message):
         From.dt_not_now = None
     else:
         m = await message.answer("Ожидайте загрузки данных (~10-20 сек).")
-
+        f = True
         From.dt_not_now = datetime.datetime.now() + relativedelta(hours=+1)
 
         dt_now = datetime.datetime.now()
@@ -96,10 +96,12 @@ async def send_list(message: types.Message):
             if 'Скоморохова Гора' in x and f_date:
                 await message.answer(x)
             else:
-                await message.answer('Нет данных о профилактических работах на <<Скоморовой Горе>> на ближайшее 2 дня.')
+                f = False
 
         await bot.delete_message(chat_id=m.chat.id, message_id=m.message_id)
 
+        if f == False:
+            await message.answer('Нет данных о профилактических работах на <<Скоморовой Горе>> на ближайшее 2 дня.')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
